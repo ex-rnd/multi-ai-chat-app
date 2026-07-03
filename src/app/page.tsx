a5a62b1b7798024a4da1b7c6004a0b2ba4776e78
James Kilonzo <jkilonzo2023@gmail.com>
2026-07-03 15:23:42 +0300

    Test: ModeToggle.tsx Dropdown Switch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
+import ModeToggle from "@/components/ModeToggle";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="dark:bg-black">
       <p>AI Chat App</p>
-      <Button className="bg-amber-300" variant={"destructive"} size={"lg"}>Hello</Button>
-
+      <ModeToggle />
     </div>
   );
 }
